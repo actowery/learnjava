799fcaaf8a9b062968661c489a07a33dc4b9b8b1
Cihan Adrian Towery <adrian.towery@gmail.com>
2018-01-28 18:42:08 -0500

    Finish problem 61

--- week3-061.NumberOfItems/src/NumberOfItems.java
+++ week3-061.NumberOfItems/src/NumberOfItems.java
@@ -3,6 +3,15 @@ import java.util.ArrayList;
 public class NumberOfItems {
 
     // implement here the method countItems
+    public static int countItems(ArrayList<String> items) {
+//I'm a fool... I could have just returned size all along..
+//        int count = 0;
+//        for (String item : items){
+//            count ++;
+//        }
+//        return count;
+        return items.size();
+    }
 
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<String>();
