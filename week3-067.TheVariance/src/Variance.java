e39ad12c6320b2a12579d1c0930192e6d558949b
Cihan Adrian Towery <adrian.towery@gmail.com>
2018-01-28 21:19:56 -0500

    Finish up to 67

--- week3-067.TheVariance/src/Variance.java
+++ week3-067.TheVariance/src/Variance.java
@@ -3,17 +3,32 @@ import java.util.ArrayList;
 public class Variance {
     // Copy here sum from exercise 63 
     public static int sum(ArrayList<Integer> list) {
-        return 0;
+        int sum = 0;
+        for (int listItem : list) {
+            sum += listItem;
+        }
+        return sum;
     }
     
     // Copy here average from exercise 64 
     public static double average(ArrayList<Integer> list) {
-        return 0;
+        return sum(list) / (double)list.size();
     }
 
     public static double variance(ArrayList<Integer> list) {
-        // write code here
-        return 0;
+        if (list.size() <= 1) {
+            System.out.println("Too small, NaN");
+            return 0;
+        }
+        double avg = average(list);
+        double temp = 0;
+        double var;
+        
+        for(int listItem : list){
+            temp += Math.pow((listItem-avg), 2);
+        }
+        var = temp / (list.size()-1);
+        return var;
     }
     
     public static void main(String[] args) {
