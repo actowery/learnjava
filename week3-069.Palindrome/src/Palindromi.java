ec962d5ce7908b83284cb4302968de7ab9f63bb9
Cihan Adrian Towery <adrian.towery@gmail.com>
2018-01-29 08:18:03 -0500

    update comments in 69

--- week3-069.Palindrome/src/Palindromi.java
+++ week3-069.Palindrome/src/Palindromi.java
@@ -3,10 +3,14 @@ import java.util.Scanner;
 public class Palindromi {
 
     public static boolean palindrome(String text) {
+//      length 0 or 1 is an automatic palindrome
         if (text.length() <= 1) {
             return true;
         }
         int end = text.length() -1;
+//      instead of copying the array in reverse as suggested, which creates 
+//      multiple iterations at worst case 2n, now we simply refer the string
+//      to itself, creating worst case n/2
         for (int i = 0; i <= end/2; i++) {
             if (text.charAt(i) != text.charAt(end-i)) {
                 return false;
